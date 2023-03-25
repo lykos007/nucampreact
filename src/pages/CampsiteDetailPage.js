11db9a9bae802c844a34cd9e6d5227aa866c9410
Leo Kontos <lykos007@yahoo.com>
2023-03-25 09:05:59 -0500

    Breadcrumbs SubHeader

--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -3,13 +3,15 @@ import { useParams } from "react-router-dom";
 import { selectCampsiteByID } from '../features/campsites/campsitesSlice';
 import CampsiteDetail from '../features/campsites/CampsiteDetail';
 import CommentsList from '../features/comments/CommentList';
+import SubHeader from '../components/SubHeader';
 
 const CampsiteDetailPage = () => {
     const { campsiteID } = useParams();
     const campsite = selectCampsiteByID(campsiteID);
     
     return (
-        <Container>
+        <Container>            
+            <SubHeader current={campsite.name} detail={true} />
             <Row>
                 <CampsiteDetail campsite={campsite} />
                 <CommentsList campsiteId={campsiteID} />
