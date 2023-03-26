41e38a131e3d89acdc89d894259380bb0ca4ddd5
Leo Kontos <lykos007@yahoo.com>
2023-03-25 20:28:57 -0500

    About Us Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import './App.css';
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='contact' element={<ContactPage />} />
+        <Route path='about' element={<AboutPage />} />
         <Route path='directory' element={<CampsitesDirectoryPage />} />
         <Route
             path='directory/:campsiteID'
