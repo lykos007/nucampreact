908a3fa7faa5b06707aeb1648d909fb9c62f4b17
Leo Kontos <lykos007@yahoo.com>
2023-04-06 12:26:27 -0500

    Redux for All

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
+import { commentsReducer} from '../features/comments/commentSlice';
+import { partnersReducer} from '../features/partners/partnersSlice';
+import { promotionsReducer} from '../features/promotions/promotionsSlice'
 
 export const store = configureStore({
   reducer: {
     campsites: campsitesReducer,
+    comments: commentsReducer,
+    partners: partnersReducer,
+    promotions: promotionsReducer
   }
 });
