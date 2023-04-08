25eee32ef8ad7999c3a07c7e1618160fbd224fea
Leo Kontos <lykos007@yahoo.com>
2023-04-08 11:18:02 -0500

    Add Login Modal

--- src/app/store.js
+++ src/app/store.js
@@ -3,7 +3,8 @@ import logger from 'redux-logger';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer} from '../features/comments/commentSlice';
 import { partnersReducer} from '../features/partners/partnersSlice';
-import { promotionsReducer} from '../features/promotions/promotionsSlice'
+import { promotionsReducer} from '../features/promotions/promotionsSlice';
+import { userReducer } from '../features/user/userSlice';
 
 
 export const store = configureStore({
@@ -11,7 +12,8 @@ export const store = configureStore({
     campsites: campsitesReducer,
     comments: commentsReducer,
     partners: partnersReducer,
-    promotions: promotionsReducer
+    promotions: promotionsReducer,
+    user: userReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
