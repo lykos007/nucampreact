26c52dbd576984c165ec54431e1cd956b84a52c7
Leo Kontos <lykos007@yahoo.com>
2023-04-12 15:49:42 -0500

    Async Campsites

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom'
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
@@ -7,8 +9,16 @@ import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());  
+  }, [dispatch]
+  );
+
   return (
     <div className="App">
       <Header />   
