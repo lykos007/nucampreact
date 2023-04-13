cee3ecc5efb6429fd19a210acd21ad0a4c8be1ad
Leo Kontos <lykos007@yahoo.com>
2023-04-12 21:24:50 -0500

    Error and Loading

--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -5,19 +5,37 @@ import { selectCampsiteByID } from '../features/campsites/campsitesSlice';
 import CampsiteDetail from '../features/campsites/CampsiteDetail';
 import CommentsList from '../features/comments/CommentList';
 import SubHeader from '../components/SubHeader';
+import Error from '../components/Error';
+import Loading from '../components/Loading';
 
 const CampsiteDetailPage = () => {
     const { campsiteID } = useParams();
     const campsite = useSelector(selectCampsiteByID(campsiteID));
     console.log('campsite:', campsite)
 
-    return (
-        <Container>            
-            <SubHeader current={campsite.name} detail={true} />
-            <Row>
+    const isLoading = useSelector((state) => state.campsites.isLoading);
+    const errMsg = useSelector((state) => state.campsites.errMsg);
+    let content = null;
+
+
+
+     if (isLoading) {
+        content = <Loading />;
+    } else if (errMsg) {
+        content = <Error errMsg={errMsg} />;
+    } else {
+        content = (
+            <>
                 <CampsiteDetail campsite={campsite} />
                 <CommentsList campsiteId={campsiteID} />
-            </Row>
+            </>
+        );
+    }
+
+    return (
+        <Container>
+            {campsite && <SubHeader current={campsite.name} detail={true} />}
+            <Row>{content}</Row>
         </Container>
     );
 };
